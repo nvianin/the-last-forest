9bb2b0e42518aebe334d4382d1abe08e559117d5
nvianin <n.vianin@gmail.com>
2022-03-31 11:57:20 +0200

    update scrapper.js

--- scrapper_REDDIT/scrapper.js
+++ scrapper_REDDIT/scrapper.js
@@ -5,4 +5,10 @@ const credentials = fs.readFileSync("./credentials", "utf-8");
 
 const r = new snoowrap({
     userAgent: "web:the-last-forest:v0.1 (by /u/TheJack77)",
+    clientId: "o3H2m8ptoKpkMLONCyeVOQ",
+    clientSecret: credentials,
+    refreshToken: "21094068-J9uGeSOePPqNG2nndgl6ljbIf_hazQ"
 })
+
+let sub = r.getSubreddit("collapse");
+log(sub)
