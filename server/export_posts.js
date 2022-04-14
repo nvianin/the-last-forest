c1d67fbf240963ce089cc126db6ea1be00f0721b
nvianin <n.vianin@gmail.com>
2022-04-14 13:02:29 +0200

    update export_posts.js

--- server/export_posts.js
+++ server/export_posts.js
@@ -14,10 +14,14 @@ this.posts = {}
 
 this.client = new MongoClient("mongodb://localhost:27017");
 this.client.connect().then(async client => {
+    log("Database connected...")
     this.db = this.client.db("last-forest");
     this.reddit_db = this.db.collection("reddit");
+    log("Collection loaded...")
     let posts = await this.reddit_db.find().toArray()
+    log("Posts fetched, loading...")
     fs.writeFileSync("./posts.json", JSON.stringify(posts));
+    log("Write complete.")
 
     // DEBUG: REMOVES THE WHOLE REDDIT DB
     /* this.reddit_db.deleteMany({}); */
