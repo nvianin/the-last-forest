fd7114e78acb611b58d00285fb5cceb417234b50
nvianin <n.vianin@gmail.com>
2022-04-24 22:42:41 +0200

    build(deps): update .package-lock.json, package-lock.json and package.json

--- server/sentiment_test.js
+++ server/sentiment_test.js
@@ -1,1 +1,19 @@
-const ml5 = require("ml5")
+const fs = require("fs");
+const {
+    SentimentAnalyzer
+} = require("node-nlp");
+const log = console.log;
+
+const posts = JSON.parse(fs.readFileSync("./posts.json", "utf-8"))
+const sentiment = new SentimentAnalyzer({
+    language: "en"
+});
+for (let p of posts) {
+    sentiment
+        .getSentiment(p.title)
+        .then(res => {
+            log("------------------")
+            log(res.vote, p.title)
+        })
+
+}
