3e8793962b299461d424b76dfbbcf53290c3f589
nvianin <n.vianin@gmail.com>
2022-04-27 19:37:30 +0200

    test: update bert_sentence.py

--- tests/bert_tests/bert_sentence.py
+++ tests/bert_tests/bert_sentence.py
@@ -1,4 +1,8 @@
+from asyncore import write
 import json
+
+from sklearn.manifold import TSNE
+
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
 
@@ -12,11 +16,21 @@ for d in data:
 # remove duplicates
 text = list(dict.fromkeys(text))
 
+
 embeddings = model.encode(text)
+keys = []
 
 for sentence, embedding in zip(text, embeddings):
+    keys.append(sentence)
     print("Sentence:", sentence)
-    print("Embedding:", embedding)
+    print("Embedding:", embedding, len(embedding))
     print("")
+
+tsne = TSNE(n_components=2, verbose=1, random_state=123);
+result = tsne.fit_transform(embeddings).tolist()
+
+print(result)
+with open("result.json", "w", encoding="utf-8") as f:
+    json.dump(result, f, ensure_ascii=False, indent=4)
 
 """ print(text); """
