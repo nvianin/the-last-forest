d2de4ca8c593ef7847cd0ec8dcc5d47dfc739250
nvianin <n.vianin@gmail.com>
2022-04-28 19:22:00 +0200

    update 3 files and delete 1 file

--- server/bert/build_tsne.py
+++ server/bert/build_tsne.py
@@ -1,54 +1,72 @@
 from asyncore import write
 import json
 
-""" from sklearn.manifold import TSNE
+from sklearn.manifold import TSNE
 
-from sentence_transformers import SentenceTransformer """
+from sentence_transformers import SentenceTransformer
 from pymongo import MongoClient;
 
-client = MongoClient("mongodb://localhost:27017")
-db = client
-print(client)
+client = MongoClient()
+posts_db = client["last-forest"].reddit
 
-# model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+posts = {}
+for doc in posts_db.find({}):
+    posts[doc["permalink"]] = doc
 
-# f = open("./posts.json", encoding="utf-8")
-# data= json.load(f)
-# print(type(data))
-
-# text = []
-# permalinks = []
-# for d in data:
-#     """ if "title" in d: """
-#     text.append(d["title"]);
-#     permalinks.append(d["permalink"])
-
-# # remove duplicates
-# """ text = list(dict.fromkeys(text)) """
+print(len(posts), "posts fetched")
 
 
-# embeddings = model.encode(text)
-# keys = []
-# i = 0;
-# for sentence, embedding in zip(text, embeddings):
-#     keys.append(sentence)
-#     print("Sentence:", sentence)
-#     print("Permalink:", permalinks[i])
-#     print("Embedding:", embedding, len(embedding))
-#     print("")
-#     i+=1
+""" f = open("./posts.json", encoding="utf-8")
+data= json.load(f)
+print(type(data)) """
 
-# tsne = TSNE(n_components=2, verbose=1, random_state=123);
-# result = tsne.fit_transform(embeddings).tolist()
-# zero_exists = False
-# for i in range(len(result)):
-#     if i == 0 : zero_exists = True
-#     print(i)
-#     result[i].append(permalinks[i])
-# """ print(result) """
-# print("Included 0:" , zero_exists)
-# with open("result.json", "w", encoding="utf-8") as f:
-#     json.dump(result, f, ensure_ascii=False, indent=4)
+text = []
+permalinks = []
+for d in posts:
+    """ print(posts[d])
+    print(posts[d]["title"]) """
+    """ if "title" in d: """
+    text.append(posts[d]["title"]);
+    permalinks.append(posts[d]["permalink"])
+
+# remove duplicates
+""" text = list(dict.fromkeys(text)) """
 
 
-# """ print(text); """
+model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+embeddings = model.encode(text)
+keys = []
+i = 0;
+for sentence, embedding in zip(text, embeddings):
+    keys.append(sentence)
+    print("Sentence:", sentence)
+    print("Permalink:", permalinks[i])
+    print("Embedding:", embedding, len(embedding))
+    print("")
+    i+=1
+
+tsne = TSNE(n_components=2, verbose=1, random_state=123);
+result = tsne.fit_transform(embeddings).tolist()
+zero_exists = False
+for i in range(len(result)):
+    if i == 0 : zero_exists = True
+    print(i)
+    result[i].append(permalinks[i])
+""" print(result) """
+print("Included 0:" , zero_exists)
+with open("result.json", "w", encoding="utf-8") as f:
+    json.dump(result, f, ensure_ascii=False, indent=4)
+
+for data in result:
+    posts_db.update_one(
+        {"permalink" : data[2]},
+        {"$set": {
+            "tsne_coordinates": {
+                "x":data[0],
+                "y":data[1]
+            }
+        }}
+    )
+
+
+""" print(text); """
