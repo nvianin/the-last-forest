6dae268a13b91bfc8ec6412e63c95a6a81af8120
nvianin <n.vianin@gmail.com>
2022-05-04 19:06:31 +0200

    test: update check_missing_fields.js

--- tests/reddit_history/check_missing_fields.js
+++ tests/reddit_history/check_missing_fields.js
@@ -3,13 +3,34 @@ const {
 } = require("mongodb")
 const log = console.log
 
+const conditions = [
+    "score",
+    "flair",
+    "tsne_coordinates"
+]
+
 const main = async () => {
     const posts = await (await new MongoClient("mongodb://localhost").connect()).db("last-forest").collection("reddit").find({}).toArray()
+    const missing = {}
+    let missing_fields = 0
+    let posts_with_incomplete_data = 0;
+
     for (post of posts) {
-        if (post.score == undefined || post.flair == undefined || ) {
-            log(post.score)
+        const missing_conditions = [];
+        for (let condition of conditions) {
+            let incomplete = false;
+            if (!post[condition]) {
+                missing_conditions.push(condition)
+                missing_fields++;
+                incomplete = true;
+            }
+            if (incomplete) posts_with_incomplete_data++;
         }
+        missing[post.permalink] = missing_conditions
     }
+
+    log(missing_conditions)
+    log()
 
 }
 
