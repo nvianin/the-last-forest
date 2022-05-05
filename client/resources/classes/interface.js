93290ad5422ca7bbe8e823f0d6cf7632ab3af01d
nvianin <n.vianin@gmail.com>
2022-05-05 14:11:06 +0200

    update interface.js

--- client/resources/classes/interface.js
+++ client/resources/classes/interface.js
@@ -1,9 +1,16 @@
 const CONTROLLER_STATES = {
-    ""
+    WALKING: "WALKING",
+    MAP: "MAP",
+    PROMENADE: "PROMENADE",
 }
 
 class AppInterface {
     constructor() {
+
+        this.state = CONTROLLER_STATES.MAP
+        this.prevState = CONTROLLER_STATES.MAP
+        this.nextState = CONTROLLER_STATES.MAP
+
         this.orbitControls = new THREE.OrbitControls(app.camera, app.renderer.domElement);
 
         this.setupListeners()
@@ -21,6 +28,20 @@ class AppInterface {
         window.addEventListener("pointerdown", e => {
 
         })
+    }
+    update() {
+        if (this.nextState != this.state) {
+            this.state = this.nextState;
+            switch (this.nextState) {
+                case CONTROLLER_STATES.PROMENADE:
+                    break;
+                case CONTROLLER_STATES.PROMENADE:
+                    break;
+                case CONTROLLER_STATES.PROMENADE:
+                    break;
+            }
+        }
+        switch ()
     }
 
     preventAutoRotate() {
