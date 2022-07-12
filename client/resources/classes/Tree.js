b67e1853ba89a4bfa8b80d6e63cf2988564d2070
nvianin <n.vianin@gmail.com>
2022-07-12 18:25:33 +0200

    update Tree.js

--- client/resources/classes/Tree.js
+++ client/resources/classes/Tree.js
@@ -193,38 +193,38 @@ const load_colors = async () => {
     const paletteShift = 12;
     /* const hexpalette = (await (await fetch("/resources/palettes/marshmellow32.hex")).text()).split("\n") */
     const hexpalette = [
-        "#2b3f41",
-        "#3a5356",
-        "#57797d",
-        "#8ca697",
-        "#3f3e20",
-        "#555735",
-        "#767f45",
-        "#a4ab79",
-        "#593234",
-        "#734141",
-        "#8c504d",
-        "#b87a66",
-        "#c1b05c",
-        "#a2c391",
-        "#907067",
-        "#71554a",
-        "#3c3c3c",
-        "#544a44",
-        "#a1623b",
-        "#b68241",
-        "#e2b55f",
-        "#b2b2f2",
-        "#00ac7c",
-        "#4ba453",
-        "#3c603f",
-        "#4e3c5c",
-        "#6e4d7e",
-        "#8f619a",
-        "#20415b",
-        "#235b7c",
-        "#2d80a6",
-        "#5eb3bc",
+        "2b3f41",
+        "3a5356",
+        "57797d",
+        "8ca697",
+        "3f3e20",
+        "555735",
+        "767f45",
+        "a4ab79",
+        "593234",
+        "734141",
+        "8c504d",
+        "b87a66",
+        "c1b05c",
+        "a2c391",
+        "907067",
+        "71554a",
+        "3c3c3c",
+        "544a44",
+        "a1623b",
+        "b68241",
+        "e2b55f",
+        "b2b2f2",
+        "00ac7c",
+        "4ba453",
+        "3c603f",
+        "4e3c5c",
+        "6e4d7e",
+        "8f619a",
+        "20415b",
+        "235b7c",
+        "2d80a6",
+        "5eb3bc",
     ]
     log(hexpalette)
     hexpalette.reverse()
@@ -237,7 +237,7 @@ const load_colors = async () => {
         col.setHSL(hsl.h, hsl.s * 1.3, hsl.l * (.86 + hsl.l ** 2))
         /* log(col) */
 
-        treeTypes[key].color = /* "#" + */ col.getHexString()
+        treeTypes[key].color = "#" + col.getHexString()
         /* log(treeTypes[key].color) */
 
         /* log(treeTypes[key]) */
